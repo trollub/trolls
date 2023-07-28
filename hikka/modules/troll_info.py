d3df6f0df49ba5b87c3cf826eb32fbbf434f7b24
trollub <140720378+trollub@users.noreply.github.com>
2023-07-28 13:39:22 +0300

    Update troll_info.py

--- hikka/modules/troll_info.py
+++ hikka/modules/troll_info.py
@@ -1,126 +1,188 @@
 
-from .. import loader, main, utils
-import logging
-import aiogram
 import git
-from aiogram.types.input_message_content import InputTextMessageContent
+from hikkatl.tl.types import Message
+from hikkatl.utils import get_display_name
 
-from telethon.utils import get_display_name
-from ..inline import GeekInlineQuery, rand
-
-logger = logging.getLogger(__name__)
+from .. import loader, utils, version
+from ..inline.types import InlineQuery
 
 
 @loader.tds
 class TrollInfoMod(loader.Module):
-    """Show userbot info (geek3.1.0alpha+)"""
+    """Show userbot info"""
 
-    strings = {
-        "name": "TrollInfo",
-        "_custom_msg_doc": "Custom message must have {owner}, {version}, {build}, {upd}, {platform} keywords",
-        "_custom_button_doc": "Custom buttons.",
-        "_photo_url_doc": "You can set your own photo to geek info.",
-        "default_message": (
-            "<b>☠️ TrollUB</b>\n\n"
-            "<b>☠️ Owner:</b> {owner}\n"
-            "<b>☠️ Version:</b> <i>{version}</i>\n"
-            "<b>☠️ Build:</b> {build}\n"
-            "<b>{upd}</b>\n\n"
-            "<b>{platform}</b>"
-        ),
-    }
-
-    def get(self, *args) -> dict:
-        return self._db.get(self.strings["name"], *args)
-
-    def set(self, *args) -> None:
-        return self._db.set(self.strings["name"], *args)
-
-    async def client_ready(self, client, db) -> None:
-        self._db = db
-        self._client = client
-        self._me = await client.get_me()
+    strings = {"name": "TrollInfo"}
 
     def __init__(self):
         self.config = loader.ModuleConfig(
-            "custom_message",
-            False,
-            lambda: self.strings("_custom_msg_doc"),
-            "custom_buttons",
-            {"text": "☠️ Support chat", "url": "https://t.me/TROLLUBChat"},
-            lambda: self.strings("_custom_button_doc"),
-            "photo_url",
-            "https://ibb.co/hHcT97z",
-            lambda: self.strings("_photo_url_doc"),
+            loader.ConfigValue(
+                "custom_message",
+                doc=lambda: self.strings("_cfg_cst_msg"),
+            ),
+            loader.ConfigValue(
+                "custom_button",
+                ["☠️TrollUB", "https://t.me/trollub"],
+                lambda: self.strings("_cfg_cst_btn"),
+                validator=loader.validators.Union(
+                    loader.validators.Series(fixed_len=2),
+                    loader.validators.NoneType(),
+                ),
+            ),
+            loader.ConfigValue(
+                "banner_url",
+                "https://github.com/FlyHost-ub/troll/blob/main/banner.png",
+                lambda: self.strings("_cfg_banner"),
+                validator=loader.validators.Link(),
+            ),
         )
 
-    def build_message(self):
-        """
-        Build custom message
-        """
+    def _render_info(self, inline: bool) -> str:
         try:
-            repo = git.Repo()
-            diff = repo.git.log(["HEAD..origin", "--oneline"])
+            repo = git.Repo(search_parent_directories=True)
+            diff = repo.git.log([f"HEAD..origin/{version.branch}", "--oneline"])
             upd = (
-                "⚠️ Update required </b><code>.update</code><b>"
-                if diff
-                else "✅ Up-to-date"
+                self.strings("update_required") if diff else self.strings("up-to-date")
             )
         except Exception:
             upd = ""
-        ver, gitlink = utils.get_git_info()
-        try:
-            return (
-                self.config["custom_message"]
-                if self.config["custom_message"]
-                else self.strings("default_message")
+
+        me = '<b><a href="tg://user?id={}">{}</a></b>'.format(
+            self._client.hikka_me.id,
+            utils.escape_html(get_display_name(self._client.hikka_me)),
+        )
+        build = utils.get_commit_url()
+        _version = f'<i>{".".join(list(map(str, list(version.__version__))))}</i>'
+        prefix = f"«<code>{utils.escape_html(self.get_prefix())}</code>»"
+
+        platform = utils.get_named_platform()
+
+        for emoji, icon in [
+            ("🍊", "<emoji document_id=5449599833973203438>🧡</emoji>"),
+            ("🍇", "<emoji document_id=5449468596952507859>💜</emoji>"),
+            ("❓", "<emoji document_id=5407025283456835913>📱</emoji>"),
+            ("🍀", "<emoji document_id=5395325195542078574>🍀</emoji>"),
+            ("🦾", "<emoji document_id=5386766919154016047>🦾</emoji>"),
+            ("🚂", "<emoji document_id=5359595190807962128>🚂</emoji>"),
+            ("🐳", "<emoji document_id=5431815452437257407>🐳</emoji>"),
+            ("🕶", "<emoji document_id=5407025283456835913>📱</emoji>"),
+            ("🐈‍⬛", "<emoji document_id=6334750507294262724>🐈‍⬛</emoji>"),
+            ("✌️", "<emoji document_id=5469986291380657759>✌️</emoji>"),
+            ("📻", "<emoji document_id=5471952986970267163>💎</emoji>"),
+        ]:
+            platform = platform.replace(emoji, icon)
+
+        return (
+            (
+                "<b>🌘 Hikka</b>\n"
+                if "hikka" not in self.config["custom_message"].lower()
+                else ""
+            )
+            + self.config["custom_message"].format(
+                me=me,
+                version=_version,
+                build=build,
+                prefix=prefix,
+                platform=platform,
+                upd=upd,
+                uptime=utils.formatted_uptime(),
+                cpu_usage=utils.get_cpu_usage(),
+                ram_usage=f"{utils.get_ram_usage()} MB",
+                branch=version.branch,
+            )
+            if self.config["custom_message"]
+            else (
+                f'<b>TrollUB</b>\n\n<b>{{}} {self.strings("owner")}:</b> {me}\n\n<b>{{}}'
+                f' {self.strings("version")}:</b> {_version} {build}\n<b>{{}}'
+                f' {self.strings("branch")}:'
+                f"</b> <code>{version.branch}</code>\n{upd}\n\n<b>{{}}"
+                f' {self.strings("prefix")}:</b> {prefix}\n<b>{{}}'
+                f' {self.strings("uptime")}:'
+                f"</b> {utils.formatted_uptime()}\n\n<b>{{}}"
+                f' {self.strings("cpu_usage")}:'
+                f"</b> <i>~{utils.get_cpu_usage()} %</i>\n<b>{{}}"
+                f' {self.strings("ram_usage")}:'
+                f"</b> <i>~{utils.get_ram_usage()} MB</i>\n<b>{{}}</b>"
             ).format(
-                owner=f'<a href="tg://user?id={self._me.id}">{get_display_name(self._me)}</a>',
-                version=utils.get_version_raw(),
-                build=f'<a href="{gitlink}">{ver[:8] or "Unknown"}</a>',
-                upd=upd,
-                platform=utils.get_platform_name(),
-            )
-        except KeyError:
-            return self.strings("default_message").format(
-                owner=f'<a href="tg://user?id={self._me.id}">{get_display_name(self._me)}</a>',
-                version=utils.get_version_raw(),
-                build=f'<a href="{gitlink}">{ver[:8] or "Unknown"}</a>',
-                upd=upd,
-                platform=utils.get_platform_name(),
-            )
-
-    async def info_inline_handler(self, query: GeekInlineQuery) -> None:
-        """
-        Send userbot info
-        @allow: all
-        """
-
-        await query.answer(
-            [
-                aiogram.types.inline_query_result.InlineQueryResultPhoto(
-                    id=rand(20),
-                    photo_url=self.config["photo_url"],
-                    title="Send userbot info",
-                    description="ℹ This will not compromise any sensitive data",
-                    caption=self.build_message(),
-                    parse_mode="html",
-                    thumb_url="https://github.com/trollub/troll/raw/main/assets/bot_pfp.png",  # noqa: E501
-                    reply_markup=self.inline._generate_markup(
-                        self.config["custom_buttons"]
+                *map(
+                    lambda x: utils.remove_html(x) if inline else x,
+                    (
+                        (
+                            utils.get_platform_emoji()
+                            if self._client.hikka_me.premium and not inline
+                            else "☠️ TrollUB"
+                        ),
+                        "<emoji document_id=5373141891321699086>😎</emoji>",
+                        "<emoji document_id=5469741319330996757>💫</emoji>",
+                        "<emoji document_id=5449918202718985124>🌳</emoji>",
+                        "<emoji document_id=5472111548572900003>⌨️</emoji>",
+                        "<emoji document_id=5451646226975955576>⌛️</emoji>",
+                        "<emoji document_id=5431449001532594346>⚡️</emoji>",
+                        "<emoji document_id=5359785904535774578>💼</emoji>",
+                        platform,
                     ),
                 )
-            ],
-            cache_time=0,
+            )
         )
 
-    async def infocmd(self, message):
-        """
-        Send userbot info
-        """
-        return await self.inline.form(
-            message=message,
-            text=self.build_message(),
-            reply_markup=self.config["custom_buttons"],
-            photo=self.config["photo_url"],
+    def _get_mark(self):
+        return (
+            {
+                "text": self.config["custom_button"][0],
+                "url": self.config["custom_button"][1],
+            }
+            if self.config["custom_button"]
+            else None
         )
+
+    @loader.inline_handler(
+        thumb_url="https://github.com/FlyHost-ub/troll/blob/main/banner.png"
+    )
+    @loader.inline_everyone
+    async def info(self, _: InlineQuery) -> dict:
+        """Send userbot info"""
+
+        return {
+            "title": self.strings("send_info"),
+            "description": self.strings("description"),
+            **(
+                {"photo": self.config["banner_url"], "caption": self._render_info(True)}
+                if self.config["banner_url"]
+                else {"message": self._render_info(True)}
+            ),
+            "thumb": (
+                "https://github.com/FlyHost-ub/troll/blob/main/banner.png"
+            ),
+            "reply_markup": self._get_mark(),
+        }
+
+    @loader.command()
+    async def infocmd(self, message: Message):
+        if self.config["custom_button"]:
+            await self.inline.form(
+                message=message,
+                text=self._render_info(True),
+                reply_markup=self._get_mark(),
+                **(
+                    {"photo": self.config["banner_url"]}
+                    if self.config["banner_url"]
+                    else {}
+                ),
+            )
+        else:
+            await utils.answer_file(
+                message,
+                self.config["banner_url"],
+                self._render_info(False),
+            )
+
+    @loader.command()
+    async def hikkainfo(self, message: Message):
+        await utils.answer(message, self.strings("desc"))
+
+    @loader.command()
+    async def setinfo(self, message: Message):
+        if not (args := utils.get_args_html(message)):
+            return await utils.answer(message, self.strings("setinfo_no_args"))
+
+        self.config["custom_message"] = args
+        await utils.answer(message, self.strings("setinfo_success"))
